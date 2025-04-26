ac47a0c811f0d894b150bf3a4d22c20dcb3ac58c
bsahd <193844131+bsahd@users.noreply.github.com>
2025-04-26 16:07:23 +0900

    PI,E,max,min and ES Modules

--- Math.js
+++ Math.js
@@ -1,5 +1,8 @@
-/**
- * Math.abs()
- * @see https://developer.mozilla.org/ja/docs/Web/JavaScript/Reference/Global_Objects/Math/abs
- */
-Math.abs = n => n > 0 ? +n : -n;
+const Math = {
+	abs: (n) => (n > 0 ? +n : -n),
+	PI: 3.14159,
+	E: 2.718,
+	max: (...n) => n.reduce((a, b) => (a < b ? b : a)),
+    min: (...n) => n.reduce((a, b) => (a > b ? b : a)),
+};
+export default Math;
