dce6b51db2f2d8aa8b44a51e66002da2fea40495
bsahd <193844131+bsahd@users.noreply.github.com>
2025-04-26 16:25:45 +0900

    Math.expを通常関数化、randomの追加

--- Math.js
+++ Math.js
@@ -1,3 +1,25 @@
+class XORShift {
+	constructor(seed = Date.now()) {
+		this.x = seed || 123456789;
+		this.y = 362436069;
+		this.z = 521288629;
+		this.w = 88675123;
+	}
+
+	next() {
+		let t = this.x ^ (this.x << 11);
+		this.x = this.y;
+		this.y = this.z;
+		this.z = this.w;
+		this.w = this.w ^ (this.w >>> 19) ^ (t ^ (t >>> 8));
+		return this.w >>> 0; // 符号なし32bit整数
+	}
+
+	nextFloat() {
+		return this.next() / 0xffffffff;
+	}
+}
+
 class Math {
 	static abs(n) {
 		return n > 0 ? +n : -n;
@@ -11,8 +33,18 @@ class Math {
 		return n.reduce((x, y) => (x > y ? y : x));
 	}
 	static pow(x, y) {
-		return x**y;
+		return x ** y;
 	}
-	static exp = (x) => this.pow(this.E, x);
+	static exp(x) {
+		return this.pow(this.E, x);
+	}
+	static random() {
+		const xs = new XORShift();
+		for (let index = 0; index < 1024; index++) {
+			xs.next();
+		}
+		return xs.nextFloat();
+	}
 }
 export default Math;
+console.log(Math.random());
