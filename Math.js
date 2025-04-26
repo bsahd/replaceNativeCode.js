1accd6143e342b6165567546dc52a34e70ee4d02
bsahd <193844131+bsahd@users.noreply.github.com>
2025-04-26 16:12:45 +0900

    クラス化

--- Math.js
+++ Math.js
@@ -1,8 +1,22 @@
-const Math = {
-	abs: (n) => (n > 0 ? +n : -n),
-	PI: 3.14159,
-	E: 2.718,
-	max: (...n) => n.reduce((a, b) => (a < b ? b : a)),
-    min: (...n) => n.reduce((a, b) => (a > b ? b : a)),
-};
+class Math {
+	static abs(n) {
+		return n > 0 ? +n : -n;
+	}
+	static PI = 3.14159;
+	static E = 2.718;
+	static max(...n) {
+		return n.reduce((x, y) => (x < y ? y : x));
+	}
+	static min(...n) {
+		return n.reduce((x, y) => (x > y ? y : x));
+	}
+	static pow(x, y) {
+		let res = x;
+		for (let index = 0; index < y - 1; index++) {
+			res = res * x;
+		}
+		return res;
+	}
+	static exp = (x) => this.pow(this.E, x);
+}
 export default Math;
