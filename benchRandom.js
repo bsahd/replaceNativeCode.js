6c636aa0d080979f77ffa70ea3fc4567d2e1b0ad
bsahd <193844131+bsahd@users.noreply.github.com>
2025-04-30 15:49:17 +0900

    format

--- benchRandom.js
+++ benchRandom.js
@@ -1,13 +1,13 @@
-import Math from "./Math.js"
-console.log("start")
-const start = performance.now()
+import Math from "./Math.js";
+console.log("start");
+const start = performance.now();
 for (let index = 0; index < 100; index++) {
-    Math.random()
+	Math.random();
 }
-console.log(`${(performance.now() - start)/100}ms per iterate`)
-console.log("start")
-const start2 = performance.now()
+console.log(`${(performance.now() - start) / 100}ms per iterate`);
+console.log("start");
+const start2 = performance.now();
 for (let index = 0; index < 100; index++) {
-    Math.random32()
+	Math.random32();
 }
-console.log(`${(performance.now() - start2)/100}ms per iterate`)
+console.log(`${(performance.now() - start2) / 100}ms per iterate`);
