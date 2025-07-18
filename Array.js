14b0738da4cebcc84d9bb552cb7ccf6732f32762
bsahd <193844131+bsahd@users.noreply.github.com>
2025-07-18 13:10:46 +0900

    フォーマットを変更

--- Array.js
+++ Array.js
@@ -1,21 +1,13 @@
 function array_reduce(array, callback, firstValue) {
-	let reduce = firstValue ?? array[0];
-	for (
-		let index = typeof firstValue == "undefined" ? 1 : 0;
-		index < array.length;
-		index++
-	) {
-		reduce = callback(reduce, array[index],index,array);
-	}
-	return reduce;
+  let reduce = firstValue ?? array[0];
+  for (let index = typeof firstValue == "undefined" ? 1 : 0; index < array.length; index++) {
+    reduce = callback(reduce, array[index], index, array);
+  }
+  return reduce;
 }
 function array_map(array, callback) {
   const res = [];
-  for (
-		let index = 0;
-		index < array.length;
-		index++
-	) {
-		res.push(callback(array[index],index,array))
-	}
+  for (let index = 0; index < array.length; index++) {
+    res.push(callback(array[index], index, array));
+  }
 }
