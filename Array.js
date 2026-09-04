d60384d8678af7a12a2d8cf4e6f772e105301a7a
bsahd <bsahd@outlook.jp>
2026-09-04 21:05:41 +0900

    array class

--- Array.js
+++ Array.js
@@ -1,13 +1,16 @@
-function array_reduce(array, callback, firstValue) {
-  let reduce = firstValue ?? array[0];
-  for (let index = typeof firstValue == "undefined" ? 1 : 0; index < array.length; index++) {
-    reduce = callback(reduce, array[index], index, array);
-  }
-  return reduce;
+class Array{
+    static reduce(array, callback, firstValue) {
+      let reduce = firstValue ?? array[0];
+      for (let index = typeof firstValue == "undefined" ? 1 : 0; index < array.length; index++) {
+        reduce = callback(reduce, array[index], index, array);
+      }
+      return reduce;
+    }
+    static map(array, callback) {
+      const res = [];
+      for (let index = 0; index < array.length; index++) {
+        res.push(callback(array[index], index, array));
+      }
+    }
 }
-function array_map(array, callback) {
-  const res = [];
-  for (let index = 0; index < array.length; index++) {
-    res.push(callback(array[index], index, array));
-  }
-}
+export default Array;
